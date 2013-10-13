3a90f8d8a4b49b106f4edfd15bcc4ce2a13001d3
Mike Hernandez <mike@culmination.org>
2013-10-12 21:59:32 -0400

    Try "not patching" the install script, also switch up the options a bit to allow for testing with stock, versions, and my custom formulae

--- luarocks.rb
+++ luarocks.rb
@@ -1,33 +1,33 @@
 require 'formula'
 
+# Warning: This is a customized formula! I'm experimenting with lua and homebrew.
+# By using this formula, you become part of my experiment ;)
+
 class Luarocks < Formula
   homepage 'http://luarocks.org'
   head 'https://github.com/keplerproject/luarocks.git'
   url 'http://luarocks.org/releases/luarocks-2.1.0.tar.gz'
   sha1 '2415bb20d6d5eff3c907512165d775b8e4088e46'
 
-  option 'with-luajit', 'Use LuaJIT instead of the stock Lua'
-  option 'with-lua52', 'Use Lua 5.2 instead of the stock Lua'
+  option 'with-stock-luajit', 'Use LuaJIT from master instead of the stock Lua (or sequethin)'
+  option 'with-luajit', 'Use LuaJIT from sequethin instead of the stock Lua, or stock luajit'
+  option 'with-lua52', 'Use Lua 5.2 from versions instead of the stock Lua (or sequethin) '
+  option 'with-stock-lua', 'Use Lua 5.2 stock Lua instead of sequethin lua'
 
-  if build.include? 'with-luajit'
+  if build.include? 'with-stock-luajit'
     depends_on 'luajit'
+  elsif build.include? 'with-luajit'
+    depends_on 'sequethin/brewtap/luajit'
   elsif build.include? 'with-lua52'
     depends_on 'lua52'
-  else
+  elsif build.include? 'with-stock-lua'
     depends_on 'lua'
+  else
+    depends_on 'sequethin/brewtap/lua'
   end
 
   fails_with :llvm do
     cause "Lua itself compiles with llvm, but may fail when other software tries to link."
-  end
-
-  # Remove writability checks in the install script.
-  # Homebrew checks that its install targets are writable, or fails with
-  # appropriate messaging if not. The check that luarocks does has been
-  # seen to have false positives, so remove it.
-  # TODO: better document the false positive cases, or remove this patch.
-  def patches
-    DATA
   end
 
   def install
@@ -62,38 +62,3 @@ class Luarocks < Formula
 end
 
 __END__
-diff --git a/src/luarocks/fs/lua.lua b/src/luarocks/fs/lua.lua
-index 67c3ce0..2d149c7 100644
---- a/src/luarocks/fs/lua.lua
-+++ b/src/luarocks/fs/lua.lua
-@@ -669,29 +669,5 @@ end
- -- @return boolean or (boolean, string): true on success, false on failure,
- -- plus an error message.
- function check_command_permissions(flags)
--   local root_dir = path.root_dir(cfg.rocks_dir)
--   local ok = true
--   local err = ""
--   for _, dir in ipairs { cfg.rocks_dir, root_dir } do
--      if fs.exists(dir) and not fs.is_writable(dir) then
--         ok = false
--         err = "Your user does not have write permissions in " .. dir
--         break
--      end
--   end
--   local root_parent = dir.dir_name(root_dir)
--   if ok and not fs.exists(root_dir) and not fs.is_writable(root_parent) then
--      ok = false
--      err = root_dir.." does not exist and your user does not have write permissions in " .. root_parent
--   end
--   if ok then
--      return true
--   else
--      if flags["local"] then
--         err = err .. " \n-- please check your permissions."
--      else
--         err = err .. " \n-- you may want to run as a privileged user or use your local tree with --local."
--      end
--      return nil, err
--   end
-+   return true
- end
